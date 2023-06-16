4c7a1615293302a724592476a864f81e93e109eb
TOFU <tamurabeta30@yahoo.co.jp>
2023-06-16 16:20:54 +0900

    finish implementation of station5

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -8,42 +8,39 @@ import { signOut } from "../authSlice";
 import { useNavigate } from "react-router-dom";
 
 const CardsLists = (props) => {
-  console.log(props)
+  console.log(props);
   return (
     <div className="cards_lists">
-      {
-        props.cards.map((e, index) => {
-          return (
-            <div className="card" key={e.id}>
-              <h2 className="card__title">{e.title}</h2>
-              <div className="url_and_reviewer">
-                <p className="card__reviewer">by {e.reviewer}</p>
-                <a className="card__url" href={e.url}>
-                  url
-                </a>
-              </div>
-              <p className="card__detail">
-                detail <br />
-                {e.detail}
-              </p>
-              <p className="card__review">
-                review <br /> {e.review}
-              </p>
+      {props.cards.map((e, index) => {
+        return (
+          <div className="card" key={e.id}>
+            <h2 className="card__title">{e.title}</h2>
+            <div className="url_and_reviewer">
+              <p className="card__reviewer">by {e.reviewer}</p>
+              <a className="card__url" href={e.url}>
+                url
+              </a>
             </div>
-          );
-        })
-      }
+            <p className="card__detail">
+              detail <br />
+              {e.detail}
+            </p>
+            <p className="card__review">
+              review <br /> {e.review}
+            </p>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export const Main = () => {
   const auth = useSelector((state) => state.auth.isSignIn);
   const navigate = useNavigate();
   const [booksList, setBooksList] = useState(null);
   const [viewOffset, setViewOffset] = useState(0);
-  const [isFirstPage, setIsFirstPage] = useState(true);
-  const [underHundredPage, setUnderHundredPage] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
   const [noContents, setNoContents] = useState(false);
   const [cookies, setCookies, removeCookie] = useCookies();
   const dispatch = useDispatch();
@@ -78,26 +75,12 @@ export const Main = () => {
   const handleViewOffset = (diff) => {
     if (diff === 0) {
       setViewOffset(0);
-      setIsFirstPage(true);
-      setUnderHundredPage(true);
-    }
-    else if (viewOffset+diff <= 0) {
+    } else if (viewOffset + diff <= 0) {
       setViewOffset(0);
-      setIsFirstPage(true);
-      setUnderHundredPage(true)
+    } else {
+      setViewOffset(viewOffset + diff);
     }
-    else if (viewOffset+diff <= 100) {
-      setViewOffset(viewOffset+diff);
-      setIsFirstPage(false);
-      setUnderHundredPage(true);
-    }
-    else {
-      setViewOffset(viewOffset+diff);
-      setIsFirstPage(false);
-      setUnderHundredPage(false);
-    }
-
-  }
+  };
 
   if (!auth)
     return (
@@ -120,48 +103,47 @@ export const Main = () => {
       <button className="logout_button" onClick={(e) => handleLogOut(e)}>
         LogOut
       </button>
-      {noContents ? (<h2>This page have no contents!</h2>) : (<></>)}
+      {noContents ? <h2>This page have no contents!</h2> : <></>}
       <div className="card_view">
         <p className="now_card_view">
           {viewOffset}~{viewOffset + 10}を表示中
         </p>
-        <CardsLists cards={booksList}/>
+        <CardsLists cards={booksList} />
       </div>
-      <div className="page_control_buttons">
-        {isFirstPage ? (<></>): (
-          <button
-            className="prev_view_offset_button"
-            onClick={() => handleViewOffset(-10)}
-          >
-            prev
+      <div className="paginator">
+        {viewOffset / 10 <= 2 ? (
+          <></>
+        ) : (
+          <button className="to_first_page" onClick={() => handleViewOffset(0)}>
+            1
           </button>
         )}
-        <button
-          className="next_view_offset_button"
-          onClick={() => handleViewOffset(10)}
-        >
-          next
-        </button>
-        <button
-          className="reset_view_offset_button"
-          onClick={() => handleViewOffset(0)}
-        >
-          reset
-        </button>
-        <button
-          className="next_hundred_view_offset_button"
-          onClick={() => handleViewOffset(100)}
-        >
-          next 100
-        </button>
-        {underHundredPage ? (<></>): (
+        {viewOffset / 10 <= 3 ? <></> : <span>...</span>}
+        {viewOffset / 10 <= 1 ? (
+          <></>
+        ) : (
           <button
-            className="prev_hundred_view_offset_button"
-            onClick={() => handleViewOffset(-100)}
+            className="prev_prev_page"
+            onClick={() => handleViewOffset(-20)}
           >
-            prev 100
+            {viewOffset / 10 - 1}
           </button>
         )}
+        {viewOffset / 10 <= 0 ? (
+          <></>
+        ) : (
+          <button className="prev_page" onClick={() => handleViewOffset(-10)}>
+            {viewOffset / 10}
+          </button>
+        )}
+        <span className="current_page">{viewOffset / 10 + 1}</span>
+        <button className="next_page" onClick={() => handleViewOffset(10)}>
+          {viewOffset / 10 + 2}
+        </button>
+        <button className="next_next_page" onClick={() => handleViewOffset(20)}>
+          {viewOffset / 10 + 3}
+        </button>
+        <span>...</span>
       </div>
     </div>
   );
